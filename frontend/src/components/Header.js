07ccbed875ec59c605da91256e86c74f742ca448
[kfirmeg] <[kfirmeg@post.bgu.ac.il]>
2022-09-10 21:07:03 +0300

    Added a space to allow a remerge

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -46,6 +46,7 @@ const LoggedInView = (props) => {
               className="user-pic pr-1"
               alt={props.currentUser.username}
             />
+            
             {props.currentUser.username}
           </Link>
         </li>
