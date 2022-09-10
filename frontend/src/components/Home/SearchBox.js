63218ee5a12c8d973e073a789bf9cd75caffe54e
[kfirmeg] <[kfirmeg@post.bgu.ac.il]>
2022-09-11 02:45:02 +0300

    added search box

---
+++ frontend/src/components/Home/SearchBox.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const SearchBox = ({ title, setTitle }) => {
+  return (
+      <input
+        id="search-box"
+        placeholder="What is it that you truly desire?"
+        type="text"
+        value={title}
+        onChange={(e) => {
+          setTitle(e.target.value);
+        }}
+      ></input>
+  );
+};
+
+export default SearchBox;
