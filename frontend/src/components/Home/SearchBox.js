e0866777efd06e111644f50ce6749dffea6770f9
[kfirmeg] <[kfirmeg@post.bgu.ac.il]>
2022-09-11 02:47:36 +0300

    lint

--- frontend/src/components/Home/SearchBox.js
+++ frontend/src/components/Home/SearchBox.js
@@ -2,15 +2,15 @@ import React from "react";
 
 const SearchBox = ({ title, setTitle }) => {
   return (
-      <input
-        id="search-box"
-        placeholder="What is it that you truly desire?"
-        type="text"
-        value={title}
-        onChange={(e) => {
-          setTitle(e.target.value);
-        }}
-      ></input>
+    <input
+      id="search-box"
+      placeholder="What is it that you truly desire?"
+      type="text"
+      value={title}
+      onChange={(e) => {
+        setTitle(e.target.value);
+      }}
+    ></input>
   );
 };
 
