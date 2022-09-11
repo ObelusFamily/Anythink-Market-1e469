e14a22bd450eda82499f38205743c1e5c3c978b1
[kfirmeg] <[kfirmeg@post.bgu.ac.il]>
2022-09-12 00:40:06 +0300

    added hide and show functionality on get in banner

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,19 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../imgs/logo.png";
 import SearchBox from "./SearchBox";
 
 const Banner = ({ title, setTitle, setShowScolding }) => {
+  const [showSearch, setShowSearch] = useState(false);
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
-          <span id="get-part">A place to get</span>
-          <SearchBox
-            title={title}
-            setTitle={setTitle}
-            setShowScolding={setShowScolding}
-          ></SearchBox>
+          <span id="get-part">
+            A place to <a onClick={() => setShowSearch(!showSearch)}>get</a>
+          </span>
+          {showSearch && (
+            <SearchBox
+              title={title}
+              setTitle={setTitle}
+              setShowScolding={setShowScolding}
+            ></SearchBox>
+          )}
           <span> the cool stuff.</span>
         </div>
       </div>
